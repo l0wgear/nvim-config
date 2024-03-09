ccd85d6bafe25dc42051c481d800d8deb27d298d
Artem Lukin <77511570+l0wgear@users.noreply.github.com>
2024-03-09 11:30:36 +0100

    Add plugins

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "catppuccin/nvim", name = "catppuccin" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
---
+++ lua/plugins/debug-python.lua
@@ -0,0 +1,62 @@
+Debug_python = "C:\\Users\\Artem\\.config\\.lsp-venv\\Scripts\\pythonw.exe"
+Default_python = "C:\\Users\\Artem\\miniconda3\\pythonw.exe"
+
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    config = function(_, opts)
+      require("mason-nvim-dap").setup(opts)
+      local dap = require("dap")
+      dap.adapters.python = function(cb, config)
+        if config.request == "attach" then
+          ---@diagnostic disable-next-line: undefined-field
+          local port = (config.connect or config).port
+          ---@diagnostic disable-next-line: undefined-field
+          local host = (config.connect or config).host or "127.0.0.1"
+          cb({
+            type = "server",
+            port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+            host = host,
+            options = {
+              source_filetype = "python",
+            },
+          })
+        else
+          cb({
+            type = "executable",
+            command = Debug_python,
+            args = { "-m", "debugpy.adapter" },
+            options = {
+              source_filetype = "python",
+            },
+          })
+        end
+      end
+      dap.configurations.python = {
+        {
+          -- The first three options are required by nvim-dap
+          type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+          request = "launch",
+          name = "Launch file",
+
+          -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+          program = "${file}", -- This configuration will launch the current file if used.
+          pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. "/venv/Scripts/pythonw.exe") == 1 then
+              return cwd .. "/venv/Scripts/pythonw.exe"
+            elseif vim.fn.executable(cwd .. "/.venv/Scripts/pythonw.exe") == 1 then
+              return cwd .. "/.venv/Scripts/pythonw.exe"
+            else
+              return Default_python
+            end
+          end,
+        },
+      }
+    end,
+  },
+}
